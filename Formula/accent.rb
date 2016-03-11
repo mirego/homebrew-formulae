0923f5a8c85eaf6cfb82e5cce9b3cefc44fe184f
Simon Prévost <sprevost@mirego.com>
2016-03-11 14:17:58 -0500

    Update accent-cli

--- Formula/accent.rb
+++ Formula/accent.rb
@@ -1,8 +1,8 @@
 class Accent < Formula
   desc "Sync, merge and export with the Accent API. Just a fancy cUrl wrapper."
   homepage "https://github.com/mirego/accent-cli"
-  url "https://github.com/mirego/accent-cli/archive/0.1.0.tar.gz"
-  sha256 "d37f6b8a506607de4aef011a3a9724f03089ed6f19cf314f4b1dafe260ce5880"
+  url "https://github.com/mirego/accent-cli/archive/0.2.0.tar.gz"
+  sha256 "00eb82851af0abbc8e1c83beb89a1639387f0dfc812722b63b936d022cf7b038"
 
   head "https://github.com/mirego/accent-cli.git"
 
