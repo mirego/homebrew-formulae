baed79e67d98d63cccbf6a4b5ffd0e5879c6a0d1
Jean-Philippe Couture <jpcouture@mirego.com>
2017-11-30 09:55:48 -0500

    Update nv to 1.0.0.

--- Formula/nv.rb
+++ Formula/nv.rb
@@ -1,8 +1,8 @@
 class Nv < Formula
   desc "Lightweight utility to load context specific environment variables"
   homepage "https://github.com/jcouture/nv"
-  url "https://github.com/jcouture/nv/archive/0.0.3.tar.gz"
-  sha256 "21eced7a32b1e96dc67a8d402f381b8da3db3124823b70911d50cf8432078346"
+  url "https://github.com/jcouture/nv/archive/1.0.0.tar.gz"
+  sha256 "ea104e05f447b8806812d1ce5eedafe06b1f4788d8daaa6ec6d6ee552682b44d"
 
   head "https://github.com/jcouture/nv.git"
 
